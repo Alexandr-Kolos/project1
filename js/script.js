f53830e90aaac1bc8fe6eb284c8cc60f5f1b008a
Alexandr <colossila@gmail.com>
2023-02-20 12:54:30 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+// alert("Hello");
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Do you have 18 years old?", "18");
+// console.log(typeof(answer));
+
+// const answers = [];
+
+// answers[0] = prompt("What is your name?", "");
+// answers[1] = prompt("What is your last name?", "");
+// answers[2] = prompt("How old are you?", "");
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`);
+
+const user = "Ivan";
+
+alert(`Hi, ${user}`);
